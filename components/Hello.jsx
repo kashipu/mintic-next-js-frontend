8fe24802eb1fb8d049aa26665a111bb9b0b58b7a
kashipu <graficowm@gmail.com>
2021-08-26 21:22:01 -0500

    Add(-) Example component and page from Laura

---
+++ components/Hello.jsx
@@ -0,0 +1,7 @@
+export default function Hello({name}) {
+    return (
+        <div>
+            <h1>Hola {name}</h1>
+        </div>
+    )
+}
---
+++ pages/laura.jsx
@@ -0,0 +1,13 @@
+import Hello from '../components/Hello'
+import HeadNav from '../components/HeadNav'
+
+export default function laura() {
+    return (
+        <div>
+            <h1 className="h1">Laura</h1>
+            <HeadNav/>
+            <Hello name={"William"}/>
+        </div>
+    )
+}
+
