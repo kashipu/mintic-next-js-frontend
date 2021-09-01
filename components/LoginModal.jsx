ad7cdf42b3e845a1acd4851f9ae50c5eb083e717
Johan Altamar <johan.altamar.dev@gmail.com>
2021-08-31 22:04:08 -0500

    Add(-): login user

--- components/LoginModal.jsx
+++ components/LoginModal.jsx
@@ -1,40 +1,98 @@
+// React - Next imports
 import { useState } from "react";
+import { useRouter } from 'next/router';
+
+// Context
+import { useAppContext } from "../context/AppContext";
+
+// Apollo dependencies
+import { gql, useMutation } from "@apollo/client";
+
+// Custom Components
 import Modal from "../components/Modal";
 import Inputs from "../components/Inputs";
-import { useAppContext } from '../context/AppContext';
+
+// Apollo MUTATION
+const LOGIN_USER = gql`
+    mutation Mutation($authenticateCredentials: CredentialsInput!) {
+        authenticate(credentials: $authenticateCredentials) {
+            access
+            refresh
+        }
+    }
+`;
+
+// Constants
+const initialState = { username: "", password: "" };
 
 export default function LoginModal() {
-    const initialState = { email: '', password: '' };
+    /* Apollo request */
+    const [loginUser, { data, error, loading }] = useMutation(LOGIN_USER);
+    // NextJS router
+    const router = useRouter();
+
     const [userInfo, setUserInfo] = useState(initialState);
 
     const { loginModal, setLoginModal } = useAppContext();
 
+    //!Pending: FIX CLOSE MODAL
     const handleCloseLoginModal = () => {
-        console.log('closing modal...')
+        console.log("closing modal...");
         setLoginModal(false);
-    }
-
-    const handleInputChange = ({target}) => {
-        const { id, value } = target;
-        setUserInfo({...userInfo, [id]: value})
     };
 
-    const handleSubmit = (event) => {
+    // Handle Input Changes
+    const handleInputChange = ({ target }) => {
+        const { id, value } = target;
+        setUserInfo({ ...userInfo, [id]: value });
+    };
+
+    // Handle user login
+    const handleSubmit = async (event) => {
         event.preventDefault();
         console.log(userInfo);
-        setUserInfo(initialState);
+
+        loginUser({
+            variables: {
+                authenticateCredentials: userInfo
+            }
+        });
+
+        if (error) {
+            // !TODO: mostrar error
+            // setErrormessage(true);
+        }
+
+        // console.log(data?.authenticate, loading, error?.message);
+        
+        /**
+         * !TODO: Aumentar el tiempo de espera de los requests. 
+         * !TODO: Mostrar los errores en el formulario.
+         * !TODO: Agregarle un loading y deshabilitar el Submit
+         */
+
+        if (data?.authenticate) {
+            const { access, refresh } = data?.authenticate;
+            // Reset form inputs
+            setUserInfo(initialState);
+            // Save tokens in localStorage
+            localStorage.setItem("accessToken", access);
+            localStorage.setItem("refreshToken", refresh);
+            // Redirect to user dashboard
+            router.push('/dashboard');
+        }
     };
 
     return (
-        loginModal && 
+        loginModal && (
             <Modal onClose={handleCloseLoginModal}>
                 <form onSubmit={handleSubmit}>
                     <Inputs
                         name="Email"
-                        id="email"
+                        id="username"
                         type="text"
                         onChange={handleInputChange}
-                        value={userInfo.email}
+                        value={userInfo.username}
                     />
                     <Inputs
                         name="Contraseña"
@@ -44,14 +102,12 @@ export default function LoginModal() {
                         value={userInfo.password}
                     />
                     <div className="button__submit">
-                        <button
-                            className="btn-nx btn-nx-primary"
-                            type="submit"
-                        >
+                        <button className="btn-nx btn-nx-primary" type="submit">
                             Ingresar
                         </button>
                     </div>
                 </form>
             </Modal>
+        )
     );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,19 @@ import '../styles/globals.css'
 import '../styles/custom.css'
 import AppContext from "../context/AppContext";
 
+// Apollo dependencies
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
+
 function MyApp({ Component, pageProps }) {
   const [loginModal, setLoginModal] = useState(false);
 
   return (
-    <AppContext.Provider value={{ loginModal, setLoginModal }}>
-      <Component {...pageProps} />
-    </AppContext.Provider>
+    <ApolloProvider client={client}>
+      <AppContext.Provider value={{ loginModal, setLoginModal }}>
+        <Component {...pageProps} />
+      </AppContext.Provider>
+    </ApolloProvider>
   )
 }
 
